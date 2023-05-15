cab090ed3961e27d6cfe9d32833534b1232428d7
Jacob Carlberg <Jacobcarlberg@outlook.com>
2023-05-15 20:34:27 +0200

    test

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-alert("test")
+export function setClassNames({ Order_Status__c }) {
+    const salesForceStatus = document.getElementById("Title-BG");
+    if (Order_Status__c == "Complete") {
+      salesForceStatus.classList.remove("service-status-default");
+      salesForceStatus.classList.add("service-status-cp");
+    } else if (Order_Status__c == "Canceled") {
+      salesForceStatus.classList.remove("service-status-default");
+      salesForceStatus.classList.add("service-status-xo");
+    } else if (Order_Status__c == "Pending Dispatch") {
+      salesForceStatus.classList.remove("service-status-default");
+      salesForceStatus.classList.add("service-status-svc");
+    } else if (Order_Status__c == "Dispatched") {
+      salesForceStatus.classList.remove("service-status-default");
+      salesForceStatus.classList.add("service-status-svc");
+    } else if (Order_Status__c == "En Route") {
+      salesForceStatus.classList.remove("service-status-default");
+      salesForceStatus.classList.add("service-status-svc");
+    } else if (Order_Status__c == "In Progress") {
+      salesForceStatus.classList.remove("service-status-default");
+      salesForceStatus.classList.add("service-status-svc");
+    } else if (Order_Status__c == "Complete - Outstanding Balance") {
+      salesForceStatus.classList.remove("service-status-default");
+      salesForceStatus.classList.add("service-status-xo");
+    }
+  }
